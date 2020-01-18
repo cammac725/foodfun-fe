676736355352509db60afbc3164fa0b14f386327
cammac <camm@rocktechcomputer.com>
2020-01-18 15:03:00 -0800

    updated Meal page

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,21 +1,51 @@
 import React from 'react'
-
-import { getMeals } from '../actions'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import moment from 'moment'
+import { getMeals, deleteMeal } from '../actions'
 
 const Meal = (props) => {
 
-  const { meal, getMeals } = props;
+  const { meal, deleteMeal, getMeals } = props;
+  const cdate = moment(meal.date_visited).format('11')
+
+  const updateHandler = () => {
+    localStorage.setItem('mealId', `${meal.id}`)
+    localStorage.setItem('itemName', `${meal.item_name}`)
+    localStorage.setItem('comment', `${meal.item_comment}`)
+    props.history.push('./update-meal')
+  }
+
+  const deleteHandler = e => {
+    e.preventDefault();
+    deleteMeal(meal.id)
+      .then(() => getMeals())
+    props.history.push('/protected')
+  }
   
   return (
     <div className='meal'>
+
       <div className='title'>
-        <h2>Meal</h2>
+        <h2>{meal.restaurant_name}</h2>
+        <p>{meal.restaurant_type}</p>
       </div>
+
       <div className='sub-title'>
-        <h3>Subtitle</h3>
+        <p>{meal.item_name}</p>
+        <img src={meal.item_photo} alt='' />
+        <p><strong>Comment:</strong>{meal.item_comment}</p>
+        <p>{cdate}</p>
+        <button onClick={updateHandler}>Update the comment</button>
+        <button onClick={deleteHandler}>Delete this meal</button>
       </div>
+
     </div>
   )
 }
 
-export default Meal
+const mapStateToProps = () => {
+  return {}
+}
+
+export default connect(mapStateToProps, { getMeals, deleteMeal })(withRouter(Meal))
